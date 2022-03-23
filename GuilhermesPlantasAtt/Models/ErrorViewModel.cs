deba25d74e7372c9d896f028947d408ea3fcd378
Arnaux Stevan <arnauxx@hotmail.com>
2022-03-23 18:38:33 -0300

    Projeto criado

---
+++ GuilhermesPlantasAtt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuilhermesPlantasAtt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
