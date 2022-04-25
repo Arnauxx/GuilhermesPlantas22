f5c300e352b7eadcd62fdcd8cf2c84f18dabff8c
Arnaux Stevan <arnauxx@hotmail.com>
2022-04-25 19:51:39 -0300

    CRUD Scaffolding

---
+++ GuilhermesPlantasAtt/Data/GuilhermesPlantasAttContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuilhermesPlantasAtt.Models
+{
+    public class GuilhermesPlantasAttContext : DbContext
+    {
+        public GuilhermesPlantasAttContext (DbContextOptions<GuilhermesPlantasAttContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GuilhermesPlantasAtt.Models.Department> Department { get; set; }
+    }
+}
