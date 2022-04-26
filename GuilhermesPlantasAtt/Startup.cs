f50f3ec45f3e53ec41eb39fe3c829debae5b58e4
Arnaux Stevan <arnauxx@hotmail.com>
2022-04-26 17:10:32 -0300

    Seeding Service

--- GuilhermesPlantasAtt/Startup.cs
+++ GuilhermesPlantasAtt/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using GuilhermesPlantasAtt.Models;
+using GuilhermesPlantasAtt.Data;
 
 namespace GuilhermesPlantasAtt
 {
@@ -39,14 +40,18 @@ namespace GuilhermesPlantasAtt
             services.AddDbContext<GuilhermesPlantasAttContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("GuilhermesPlantasAttContext"), builder =>
                     builder.MigrationsAssembly("GuilhermesPlantasAtt")));
+
+            services.AddScoped<SeedingService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
